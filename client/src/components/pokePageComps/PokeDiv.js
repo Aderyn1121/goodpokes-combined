0e3f11f0a42bd61f815c400de652a7949ac6746b
Addy <zaristus@gmail.com>
2020-06-14 18:42:13 -0700

    header type

--- client/src/components/pokePageComps/PokeDiv.js
+++ client/src/components/pokePageComps/PokeDiv.js
@@ -140,6 +140,8 @@ class PokeDiv extends Component {
                         <button className='ui button blue' id='water' onClick={() => { this.handleType('water') }}>WATER</button>
                     </nav>
 
+                    <h1 className='ui inverted segment' style={{ fontSize: '20px', textDecoration: 'bold', padding: '10px 0', textAlign: 'center' }}>All</h1>
+
                     <nav className='3 fluid ui buttons'>
                         <button className='ui button green' onClick={this.handlePrevious}>Previous</button>
                         <button className='ui button primary' onClick={this.handleNext}>Next</button>
@@ -154,39 +156,45 @@ class PokeDiv extends Component {
         }
 
 
-        else return (
-            <div>
-                <nav className='18 fluid ui buttons'>
-                    <button className='ui button green' id='bug' onClick={() => { this.handleType('bug') }}>BUG</button>
-                    <button className='ui button purple' id='dark' onClick={() => { this.handleType('dark') }}>DARK</button>
-                    <button className='ui button yellow' id='electric' onClick={() => { this.handleType('electric') }} >ELECTRIC</button>
-                    <button className='ui button purple' id='dragon' onClick={() => { this.handleType('dragon') }}>DRAGON</button>
-                    <button className='ui button pink' id='fairy' onClick={() => { this.handleType('fairy') }}>FAIRY</button>
-                    <button className='ui button orange' id='fighting' onClick={() => { this.handleType('fighting') }}>FIGHTING</button>
-                    <button className='ui button red' id='fire' onClick={() => { this.handleType('fire') }}>FIRE</button>
-                    <button className='ui button teal' id='flying' onClick={() => { this.handleType('flying') }}>FLYING</button>
-                    <button className='ui button' id='normal' onClick={() => { this.handleType('normal') }}>NORMAL</button>
-                    <button className='ui button purple' id='poison' onClick={() => { this.handleType('poison') }}>POISON</button>
-                    <button className='ui button pink' id='psychic' onClick={() => { this.handleType('psychic') }}>PSYCHIC</button>
-                    <button className='ui button purple' id='ghost' onClick={() => { this.handleType('ghost') }}>GHOST</button>
-                    <button className='ui button green' id='grass' onClick={() => { this.handleType('grass') }}>GRASS</button>
-                    <button className='ui button orange' id='rock' onClick={() => { this.handleType('rock') }}>ROCK</button>
-                    <button className='ui button black' id='steel' onClick={() => { this.handleType('steel') }}>STEEL</button>
-                    <button className='ui button orange' id='ground' onClick={() => { this.handleType('ground') }}>GROUND</button>
-                    <button className='ui button teal' id='ice' onClick={() => { this.handleType('ice') }}>ICE</button>
-                    <button className='ui button blue' id='water' onClick={() => { this.handleType('water') }}>WATER</button>
-                </nav>
-                <nav className='3 fluid ui buttons'>
-                    <button className='ui button green' onClick={this.handlePrevious}>Previous</button>
-                    <button className='ui button primary' onClick={this.handleNext}>Next</button>
-                </nav>
-                <PokeListType list={this.state.listSegment} />
-                <nav className='3 fluid ui buttons'>
-                    <button className='ui button green' onClick={this.handlePrevious}>Previous</button>
-                    <button className='ui button primary' onClick={this.handleNext}>Next</button>
-                </nav>
-            </div >
-        )
+        else {
+
+            return (
+                <div>
+                    <nav className='18 fluid ui buttons'>
+                        <button className='ui button green' id='bug' onClick={() => { this.handleType('bug') }}>BUG</button>
+                        <button className='ui button purple' id='dark' onClick={() => { this.handleType('dark') }}>DARK</button>
+                        <button className='ui button yellow' id='electric' onClick={() => { this.handleType('electric') }} >ELECTRIC</button>
+                        <button className='ui button purple' id='dragon' onClick={() => { this.handleType('dragon') }}>DRAGON</button>
+                        <button className='ui button pink' id='fairy' onClick={() => { this.handleType('fairy') }}>FAIRY</button>
+                        <button className='ui button orange' id='fighting' onClick={() => { this.handleType('fighting') }}>FIGHTING</button>
+                        <button className='ui button red' id='fire' onClick={() => { this.handleType('fire') }}>FIRE</button>
+                        <button className='ui button teal' id='flying' onClick={() => { this.handleType('flying') }}>FLYING</button>
+                        <button className='ui button' id='normal' onClick={() => { this.handleType('normal') }}>NORMAL</button>
+                        <button className='ui button purple' id='poison' onClick={() => { this.handleType('poison') }}>POISON</button>
+                        <button className='ui button pink' id='psychic' onClick={() => { this.handleType('psychic') }}>PSYCHIC</button>
+                        <button className='ui button purple' id='ghost' onClick={() => { this.handleType('ghost') }}>GHOST</button>
+                        <button className='ui button green' id='grass' onClick={() => { this.handleType('grass') }}>GRASS</button>
+                        <button className='ui button orange' id='rock' onClick={() => { this.handleType('rock') }}>ROCK</button>
+                        <button className='ui button black' id='steel' onClick={() => { this.handleType('steel') }}>STEEL</button>
+                        <button className='ui button orange' id='ground' onClick={() => { this.handleType('ground') }}>GROUND</button>
+                        <button className='ui button teal' id='ice' onClick={() => { this.handleType('ice') }}>ICE</button>
+                        <button className='ui button blue' id='water' onClick={() => { this.handleType('water') }}>WATER</button>
+                    </nav>
+
+                    <h1 className='ui inverted segment' style={{ fontSize: '20px', textDecoration: 'bold', padding: '10px 0', textAlign: 'center' }}>{this.state.type}</h1>
+
+                    <nav className='3 fluid ui buttons'>
+                        <button className='ui button green' onClick={this.handlePrevious}>Previous</button>
+                        <button className='ui button primary' onClick={this.handleNext}>Next</button>
+                    </nav>
+                    <PokeListType list={this.state.listSegment} />
+                    <nav className='3 fluid ui buttons'>
+                        <button className='ui button green' onClick={this.handlePrevious}>Previous</button>
+                        <button className='ui button primary' onClick={this.handleNext}>Next</button>
+                    </nav>
+                </div >
+            )
+        }
     }
 }
 
